a41e8bf6eff312412f57862dde194a312ac2a463
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2017-07-10 00:13:06 +0100

    Move plot_year function to utils

--- utils.R
+++ utils.R
@@ -20,3 +20,19 @@ draw_ggmap = function(location, zoom, data) {
     scale_fill_gradient(low = "green", high = "red") +
     scale_alpha(range = c(0, 0.3), guide = FALSE)
 }
+
+UK = get_map(location = c(-4.121072, 53.919044), zoom = 6, maptype = "roadmap")
+
+plor_year = function(y) {
+  '
+  Function to draw a ggmap of the UK with the yearly data points on top of it 
+  y: year of interest
+  '
+  sub = ct %>% 
+    filter(year == y)
+  
+  ggmap(UK) +
+    geom_point(data = sub, aes(x = lon, y = lat), alpha = 0.2, color = "red", size = .5) +
+    ggtitle(y) + 
+    theme(plot.title = element_text(hjust = 0.5))
+}
