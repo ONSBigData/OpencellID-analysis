01dab5dda1a6c3252cc959bdcac9b2642b1e6aa2
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2017-08-07 17:21:35 +0100

    Rename files

--- execute.R
+++ execute.R
@@ -14,3 +14,11 @@ ezknit(file = "analysis/Part_3_APPENDIX-B.Rmd",
        out_dir = "reports/part3",
        fig_dir = "figures",
        keep_html = FALSE)
+
+mdList = list.files(pattern="*.\\.md", recursive = TRUE)
+getNewPath = function(oldPath) {
+  dirPath = dirname(oldPath)
+  newPath = file.path(dirPath, "README.md")
+  return(newPath)
+}
+file.rename(mdList, sapply(mdList, getNewPath, simplify = "array"))
